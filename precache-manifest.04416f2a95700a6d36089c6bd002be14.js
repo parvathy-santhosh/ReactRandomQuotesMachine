50c907b8c5985c7640bb47e8dd4cd5bf20c488ea
Parvathy Santhosh <parsan1998@gmail.com>
2018-11-06 21:56:40 +0530

    Updates

---
+++ precache-manifest.04416f2a95700a6d36089c6bd002be14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d569ca176a4999705b4",
+    "url": "/ReactRandomQuotesMachine/static/js/runtime~main.1d569ca1.js"
+  },
+  {
+    "revision": "f52465ec389b964bcf45",
+    "url": "/ReactRandomQuotesMachine/static/js/main.f52465ec.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/ReactRandomQuotesMachine/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "f52465ec389b964bcf45",
+    "url": "/ReactRandomQuotesMachine/static/css/main.9a26117c.chunk.css"
+  },
+  {
+    "revision": "bcdce1f598be129773e1d0376c920393",
+    "url": "/ReactRandomQuotesMachine/index.html"
+  }
+];
